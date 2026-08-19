57bf5052d73f7eaa0c51bec49b00f7b299cefb84
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-08-19 12:23:38 -0300

    feat: definir boilerplate do spring

---
+++ src/main/java/com/github/svitorz/sells/SellsApplication.java
@@ -0,0 +1,13 @@
+package com.github.svitorz.sells;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/svitorz/sells/SellsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.svitorz.sells;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
