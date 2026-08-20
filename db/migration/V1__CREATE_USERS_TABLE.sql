41b445be591d62ddbfd0b66f2f1a7701e2ff0d9f
Vitor Fábio <vitor.fabio.castro@gmail.com>
2026-08-20 17:13:23 -0300

    sql: criar tabela users

---
+++ db/migration/V1__CREATE_USERS_TABLE.sql
@@ -0,0 +1,11 @@
+create table users(
+  id BIGINT,
+  name varchar(100) not null,
+  email varchar(100) not null,
+  password varchar(255) not null,
+  role varchar(50) not null,
+  created_at TIMESTAMP,
+  update_at TIMESTAMP,
+  deleted_at TIMESTAMP,
+  CONSTRAINT PRIMARY KEY(id)
+);
