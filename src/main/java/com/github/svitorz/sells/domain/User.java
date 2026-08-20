b44345b96b53ec80906911106ee7bc21c105d1eb
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-08-20 15:32:13 -0300

    feat: implementar entidade User com auditoria

---
+++ src/main/java/com/github/svitorz/sells/domain/User.java
@@ -0,0 +1,40 @@
+package com.github.svitorz.sells.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "user")
+@SQLDelete(sql = "UPDATE user SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+public class User extends Auditable{
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "id" )
+    private UUID id;
+
+    @NotBlank
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @NotBlank
+    @Column(length = 255, nullable = false)
+    private String password;
+}
